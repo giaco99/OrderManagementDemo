46cd2e750a039e1b6e40c355527781ab38b224dc
giaco99 <62729639+giaco99@users.noreply.github.com>
2023-12-03 12:01:01 +0100

    Add files via upload

---
+++ ProductMicroService.Models/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductMicroService.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public int CategoryId { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public virtual ProductCategory Category { get; set; }
+    }
+}
---
+++ ProductMicroService.Models/Models/ProductCategory.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductMicroService.Models
+{
+    public class ProductCategory
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
